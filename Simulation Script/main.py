2497263ca3a1a89b7b9c425173094650860e3cff
HrushikeshAnandSarangi <hrushikeshsarangi7@gmail.com>
2025-09-09 08:05:18 +0530

    Script Updated and redundant data removed

--- Simulation Script/main.py
+++ Simulation Script/main.py
@@ -1,12 +1,14 @@
 import time
 import socket
 
-# Path to your .txt file
-file_path = "data.txt"
+file_paths = [
+    "data/train_FD001.txt",
+    "data/train_FD002.txt",
+    "data/train_FD003.txt"
+]  
 
-# Socket configuration
-HOST = "127.0.0.1"  # Localhost
-PORT = 9999         # Port to send data
+HOST = "127.0.0.1"
+PORT = 9999
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((HOST, PORT))
@@ -16,15 +18,15 @@ conn, addr = sock.accept()
 print(f"Connected by {addr}")
 
 try:
-    with open(file_path, 'r') as f:
-        for line in f:
-            data_str = line.strip()
-            if not data_str:
-                continue  # skip empty lines
-
-            conn.sendall(data_str.encode() + b'\n')
-            print(f"Sent: {data_str}")
-            time.sleep(5)
+    for file_path in file_paths:
+        with open(file_path, 'r') as f:
+            for line in f:
+                data_str = line.strip()
+                if not data_str:
+                    continue
+                conn.sendall(data_str.encode() + b'\n')
+                print(f"Sent: {data_str}")
+                time.sleep(5)
 finally:
     conn.close()
     sock.close()
